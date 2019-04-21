e81729b0c995ff09ef215aa6d3d6de4025a3970a
nofear195 <aaasss777as@gmail.com>
2019-04-21 13:10:16 +0800

    test2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,4 +1,10 @@
 function capitalize(str) {
+  const diff = 'a'.charCodeAt(0) - 'A'.charCodeAt(0);
+  let newStr;
+  if (str[0] >= 'a' && str[0] <= 'z') {
+    newStr = str.replace(str[0], String.fromCharCode(str.charCodeAt(0) - diff));
+    return newStr;
+  }
   return str;
 }
 
