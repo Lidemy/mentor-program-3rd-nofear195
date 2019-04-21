5df5de8b2b6c531c3c5f3b1adfeb927420ca4423
nofear195 <aaasss777as@gmail.com>
2019-04-21 14:38:09 +0800

    test5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,26 @@
 function join(str, concatStr) {
-  return concatStr;
+  let i;
+  let newStr = '';
+  let newStr2 = '';
+  for (i = 0; i < str.length; (i += 1)) {
+    newStr += str[i] + concatStr;
+  }
+  if (newStr.length > str.length) {
+    for (i = 0; i < (newStr.length - 1); (i += 1)) {
+      newStr2 += newStr[i];
+    }
+    return newStr2;
+  }
+  return newStr;
 }
 
 function repeat(str, times) {
-  return times;
+  let i;
+  let newStr = '';
+  for (i = 1; i <= times; (i += 1)) {
+    newStr += str;
+  }
+  return newStr;
 }
 
 console.log(join('a', '!'));
