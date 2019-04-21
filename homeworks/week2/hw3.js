c6ca73d0429511988ba5517cc88ed3bb7968af8c
nofear195 <aaasss777as@gmail.com>
2019-04-21 13:30:46 +0800

    test3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  let newStr = ' ';
+  let i;
+  for (i = (str.length - 1); i >= 0; (i -= 1)) {
+    newStr += str[i];
+  }
+  return newStr;
 }
 
 reverse('hello');
